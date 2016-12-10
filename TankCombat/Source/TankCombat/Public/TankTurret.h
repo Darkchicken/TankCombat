d48e098b0f2906f29de613b5d95edfe698ba21ad
Darkchicken <joshshucker@hotmail.com>
2016-12-10 12:47:02 -0800

    Fixed collision glitches
    Disabled collisions on turret and barrel (stops bouncing glitch)

--- TankCombat/Source/TankCombat/Public/TankTurret.h
+++ TankCombat/Source/TankCombat/Public/TankTurret.h
@@ -8,7 +8,7 @@
 /**
  * 
  */
-UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+UCLASS(meta = (BlueprintSpawnableComponent))//, hidecategories = ("Collision"))
 class TANKCOMBAT_API UTankTurret : public UStaticMeshComponent
 {
 	GENERATED_BODY()
