6789cc6ce63eee1481b39163b3b7eb6c12113132
Darkchicken <joshshucker@hotmail.com>
2016-12-10 11:33:22 -0800

    Set up firing
    Created a fire function in Tank
    Set up input mappings for firing
    Set up fire to be called from Tank blueprint
    Created a Projectile class with an associated actor blueprint

--- TankCombat/Source/TankCombat/Public/Tank.h
+++ TankCombat/Source/TankCombat/Public/Tank.h
@@ -14,13 +14,16 @@ class TANKCOMBAT_API ATank : public APawn
 {
 	GENERATED_BODY()
 public:
-	void AimAt(FVector HitLocation);
-
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetTurretReference(UTankTurret* TurretToSet);
+
+	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Firing)
+	void Fire();
 
 protected:
 
