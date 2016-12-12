333019254a74e1f8d0e7d6b0229e3f351637d5ca
Darkchicken <joshshucker@hotmail.com>
2016-12-12 14:15:00 -0800

    Forward and backward movement functional
    Fly by wire controls for forward movement now works on both gamepad and
    keyboard

--- TankCombat/Source/TankCombat/Private/TankMovementComponent.cpp
+++ TankCombat/Source/TankCombat/Private/TankMovementComponent.cpp
@@ -1,12 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankCombat.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	//TODO prevent extra speed from double control use (clamp throw?)
 }
 
